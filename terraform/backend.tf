c79675ba6d7a762def139fcf48466874a6afbcc1
Sabrin_Jadallah <112097026+sabrinajad@users.noreply.github.com>
2023-04-20 15:20:35 +0200

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eks-helm9"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    # namodb_table = "lock"
-  }  
-}
--- terraform/elastic_ip.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_eip" "nat" {
-  vpc = true
-
-  tags = {
-    Name = "nat"
-  }
-}
